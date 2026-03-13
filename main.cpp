114ea26c9c666b3966b0a6dc19f0869d508c0297
Legitmod17 <neshkin2018@yandex.ru>
2026-03-13 16:55:28 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+	int choice, type, size, length;
+	char texture;
+
+	cout << "Программа - \"Геометрические формы\"\n\n";
+	cout << "Линия\n";
+	cout << "Квадрат\n";
+	cout << "Прямоугольник\n";
+	cout << "Выберите фигуру\n";
+	cin >> choice;
+
+	if (choice == 1) {
+	system("cls");
+
+	} 
+
